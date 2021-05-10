e7f1c36001ca86604f596bdb78bb9af2af84af17
Pro_Fire_Development <zakm4@educbe.ca>
2021-05-10 14:52:54 -0600

    made changes to update system - still broken

--- Pi game stuff/breakout-game.py
+++ Pi game stuff/breakout-game.py
@@ -4,9 +4,11 @@ import math
 import sys
 
 from pygame import time
+from pygame import draw
 from pygame import event
 from pygame import display
-from pygame.constants import QUIT
+from pygame.constants import K_LEFT, K_RIGHT, QUIT
+from pygame import key
 
 
 class Game():
@@ -21,27 +23,77 @@ class Game():
     def update(self):
         pass
 
+################___missing some stuff here____###############################
+# still broken, watch / read later
+
 # there might be an erroe copying and pasting here
 class Ball():
+
+    colour = "red"
+    radius = 15
+    targetVelocity = 0.5 #change number for ball speed
+
+#logic for the ball bouncing off the padel and moving around
 
     def __init__(self,game):
 
         self.Window = game.Window
+        self.resetBall()
+        self.maxLeft = self.radius
+        self.maxRight = self.Window.get_width() - self.radius
+        self.maxHight = 100 + self.radius
 
-    def update(self):
-        pass
+    def draw(self):
+        draw.circle(self.Window,self.colour,(self.x,self.y),self.radius)
+
+    def update(self,dt):
+        deltaX = self.velX * dt
+        deltaY =self.velY *dt
+
+        self.X += deltaX
+
+        if self.X < self.maxLeft:
+            self.X = self.maxLeft + (self.maxLeft - self.X)
+            self.velX *= -1
+        
+        if self.X < self.maxRight:
+            self.X = self.maxRight + (self.maxRight - self.X)
+            self.velX *= -1
+        
+        self.y +- deltaY
+        
+        if self.y < self.maxHight:
+            self.y = 2*self.maxHight - self.y
+            self.velY *= -1
+
+        if self.y > 800:
+            self.resetBall()
+
 
     def resetBall(self):
-        pass
+        self.x = 600
+        self.y = 600
 
+        angle = random.uniform(0.5,math.pi-0.5) # angle in radians not digrees - using math lib
+
+        self.velX = self.targetVelocity * math.cos*angle
+        self.velY = -self.targetVelocity * math.cos*angle
 class paddle():
 
     def __init__(self,game):
 
         self.Window = game.Window
 
-    def update(self):
-        pass
+    def update(self,dt,keys):
+        if keys[K_LEFT]:
+            velocity = -0.8
+        elif keys[K_RIGHT]:
+             velocity = 0.8
+        else:
+             velocity = 0
+            
+        delta =  velocity*dt
+        self.x += delta
 
 class Brick():
 
@@ -67,14 +119,13 @@ while True:
             #quit()
             sys.exit()
 
-
+#start of the game components being called
 
     dt = clock.tick(60) #frame rate counter
+    keys = key.get_pressed()
 
     game.Window.fill("gray60")
 
-    #update
-
-    #draw
-
+    game.update(dt)
+    game.draw()
     display.update()
