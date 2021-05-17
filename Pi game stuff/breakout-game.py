c92d0920de216f1e5d5e3b23376a070fa8b47929
Pro_Fire_Development <zakm4@educbe.ca>
2021-05-17 15:02:54 -0600

    un screwed some stuff

--- Pi game stuff/breakout-game.py
+++ Pi game stuff/breakout-game.py
@@ -2,12 +2,15 @@ import pygame
 import random
 import math
 import sys
-
+ # that amazing moment when you somehow manage to break everything for no reason at all
+ # but then you realizse that its just your own pure stupidity why its not working
+ ## fix later - installed a temp fix, but game currently will not rn
+ #check back on #2 to review code around Ln 19 and 100
 from pygame import time
 from pygame import draw
 from pygame import event
 from pygame import display
-from pygame.constants import K_LEFT, K_RIGHT, QUIT
+from pygame.constants import K_LEFT, K_RIGHT, K_x, QUIT
 from pygame import key
 
 
@@ -23,20 +26,25 @@ class Game():
     def update(self):
         pass
 
+    def bounce(self,Object):
+
+        if Object.x <= Object.x + Object.length:
+            self.Ball.velY *= -1
+        elif Object.y <= self.Ball.y <= Object.y + Object.width:
+
+
 ################___missing some stuff here____###############################
 # still broken, watch / read later
 
 # there might be an erroe copying and pasting here
-class Ball():
-
-    colour = "red"
-    radius = 15
-    targetVelocity = 0.5 #change number for ball speed
+         class Ball():
+            colour = "red"
+            radius = 15
+            targetVelocity = 0.5 #change number for ball speed
 
 #logic for the ball bouncing off the padel and moving around
 
     def __init__(self,game):
-
         self.Window = game.Window
         self.resetBall()
         self.maxLeft = self.radius
@@ -83,7 +91,10 @@ class paddle():
     def __init__(self,game):
 
         self.Window = game.Window
-
+        self.x =540
+        self.y = 540
+        #self.maxRight =
+        #self.rect.update(self.x,self.y,self.length,self.width)
     def update(self,dt,keys):
         if keys[K_LEFT]:
             velocity = -0.8
@@ -94,9 +105,11 @@ class paddle():
             
         delta =  velocity*dt
         self.x += delta
-
+        #self.rect.update(self.x,self.y,self.length,self.width)
 class Brick():
 
+
+# fix bounce function aswell as add in collide rect from pi game here
     def __init__(self,game):
 
         self.Window = game.Window
@@ -129,3 +142,14 @@ while True:
     game.update(dt)
     game.draw()
     display.update()
+
+
+    # fix ball pos locking - very broken
+
+    # add in smoother movements -- its no where near close to being "smooth"
+
+
+
+
+    # it should be working now, but its not 🤔 i am confused now -- somethning to do
+    # with the missing windows class - it runs sometimes, but not
