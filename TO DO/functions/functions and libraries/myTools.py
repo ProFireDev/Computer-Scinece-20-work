dfb56234450d8047aa8d2ff73b3ab31e38203c78
Pro_Fire_Development <zakm4@educbe.ca>
2021-06-12 17:58:02 -0600

    woo! almost there

--- TO DO/functions/functions and libraries/myTools.py
+++ TO DO/functions/functions and libraries/myTools.py
@@ -14,53 +14,55 @@ def getNumber(Msg, Min, Max, Type):
         print("improper type, please enter a float or int")
         getNumber() #reruns the function to reload it
 
+import math
 
-
-#very much not working currently (type error)
 def makeChange(Amount):
       try:
-          c=int(Amount) # c = change
+          NewAmount =math.modf(Amount)
+          #dec first, doller second
+          b=int(Amount[1]) # b = bills
+            #bills
+          hundred = b//100.00
+          b = b%100.00
+          fifty  = b//50.00
+          b = b%50.00
+          twenty = b//20.00
+          b = b%20.00
+          ten  = b//10.00
+          b = b%10.00
+          five  = b//5.00
+          b = b%5.00
+          one  = b//1.00
+          b = b%1.00
 
           #coins
-          quarters = c//0.25,
+          c=float(NewAmount[0])
+          quarters = c// 0.25
           c = c%0.25
-          dimes = c//0.10,
+          dimes = c//0.10
           c = c%0.10
-          nickels = c//0.05,
+          nickels = c//0.05 
           c = c%0.05
-          pennies = c//0.01,
-
-          #bills
-          hundred = c//100.00,
-          c = c%100.00
-          fifty  = c//50.00,
-          c = c%50.00
-          twenty = c//20.00,
-          c = c%20.00
-          ten  = c//10.00,
-          c = c%10.00
-          five  = c//5.00,
-          c = c%5.00
-          one  = c//1.00,
-          c = c%1.00
+          pennies = c//0.01
+          c =c%0.05
 
           #printing out of the values:
 
           Dvalues = f" the change is:\n DOllARS:\n $1: {one}\n $5: {five}\n $10: {ten}\n $20: {twenty}\n $50: {fifty}\n $100: {hundred}"
-          Cvalues =f"CENTS:\n $0.01: {pennies}\n $0.05: {nickels}\n $0.10: {dimes}\n 0.25 {quarters}"
+          Cvalues =f"CENTS:\n $0.01: {pennies}\n $0.05: {nickels}\n $0.10: {dimes}\n $0.25: {quarters}"
 
           results = Dvalues + "\n" + Cvalues
-    
-          return results
+          #return results
+          print(results)
       except:
          error = input("an error has occoured counting\n are you sure you entered a string?\n try again? (Y or N)")
          if(error=="Y"):
-             makeChange()
+             try:
+              makeChange()
+             except:
+                 print("exit code 1, a fatal error has occurred")
          else:
              quit()
-
-
-
 
 def getChange(Owed, Paid):
     #fix code above, then just take the diffrence here
