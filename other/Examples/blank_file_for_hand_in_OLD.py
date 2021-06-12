7e39560c4ecfec1942b79a3186a8bb6ef537ed9b
Pro_Fire_Development <zakm4@educbe.ca>
2021-06-11 23:42:15 -0600

    Update blank_file_for_hand_in_OLD.py

--- other/Examples/blank_file_for_hand_in_OLD.py
+++ other/Examples/blank_file_for_hand_in_OLD.py
@@ -1,7 +1,7 @@
 
 print("#blank file upload, ignore it\n click on the link instead")
 
-print("https://github.com/ProFireDev/CS-20-handin")
+print("https://github.com/ProFireDev/Computer-Scinece-20-work/tree/main/Assignments/.Q4%20current")
 #blank file upload, ignore it
 
 print("all of the files are located there")
