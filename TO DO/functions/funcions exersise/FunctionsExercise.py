e61ce3a5ef07d7d07cd715953326bf6c13635472
Pro_Fire_Development <zakm4@educbe.ca>
2021-06-12 11:56:19 -0600

    added in stuff to return

--- TO DO/functions/funcions exersise/FunctionsExercise.py
+++ TO DO/functions/funcions exersise/FunctionsExercise.py
@@ -1,29 +1,35 @@
 
+#1) Write a function called subNumbers that subtracts 2 numbers and returns the
+#   result. Note, if given decimal numbers, it should round the result to 3 decimal
+#   places. (The round() function will be useful here)
 def subNumbers(num1,num2):
     output = num1 - num2
     round(output, 3)
+    return output
 
 def  isEven(Number):
     if (Number % 2) == 0:
-        Number = True
+        return(True)
     else:
-      Number = False
+      return(False)
 
 def  isCapitalized(Word):
     if(Word.isupper()):
-        Word = True
+        return(True)
     else:
-        Word = False
-
+        return(False)
 def  isMultiple(Number, MultipleOf):
     if (Number % MultipleOf == 0):
-        Number = True
+        return(True)
     else:
-        Number = False
+        return(False)
+
 
 def Sum(UpTo, StartFrom):
+    
     return
 
 def datingRange(Age):
      lower = (Age/2)+7
-     oldest =(Age*2)-7 #thats proper math right?
+     oldest =(Age-7)*2 #thats proper math right?
+     return (int(lower)),((int(oldest)))
