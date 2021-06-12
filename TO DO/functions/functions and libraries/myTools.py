a4088a27c921ba9c0050027b0036e2c6055604f2
Pro_Fire_Development <zakm4@educbe.ca>
2021-06-12 14:03:11 -0600

    more kinda broken stuff lol

--- TO DO/functions/functions and libraries/myTools.py
+++ TO DO/functions/functions and libraries/myTools.py
@@ -15,22 +15,46 @@ def getNumber(Msg, Min, Max, Type):
         getNumber() #reruns the function to reload it
 
 
+
+#very much not working currently (type error)
 def makeChange(Amount):
       #do math to figure out bills and coins:
     try:
-     Amount = () # not sure if pennys count , because they dont exist anymore, but included just in case
+     Amount = (Amount) # not sure if pennys count , because they dont exist anymore, but included just in case
      for change in [0.25,0.10,0.50,0.10,5.00,10.00,20.00,50.00,100.00]:
         num = Amount/change
         Amount += (change,) * num
         Amount -= change * num
+        print(Amount) #confusing names here
     #uhhh math is hard
-  
     except:
         print("A fatal error has occoured!")
         print(f"the value entered is not a float!\n{Amount}!")
 
-def getChange():
+
+
+def getChange(Owed, Paid):
+    #fix code above, then just take the diffrence here
     return
 
-def isPrime():
-    return
+
+
+# used algorithum
+def isPrime(Number):
+     # We know 1 is not a prime number
+    if Number == 1:
+        return False
+
+    i = 2
+    # This will loop from 2 to int(sqrt(x))
+    while i*i <= Number:
+        # Check if i divides x without leaving a remainder
+        if Number % i == 0:
+            # This means that n has a factor in between 2 and sqrt(n)
+            # So it is not a prime number
+            return False
+            
+        i += 1
+    # If we did not find any factor in the above loop,
+    # then n is a prime number
+    return True
