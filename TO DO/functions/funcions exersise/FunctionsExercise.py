d38d91e09b0d91f452fddd1a8b96068a8b2b94df
ProFireDev <zakm4@educbe.ca>
2021-06-12 03:08:24 -0600

    im gonna sleep and pray this is accepted tom aft

--- TO DO/functions/funcions exersise/FunctionsExercise.py
+++ TO DO/functions/funcions exersise/FunctionsExercise.py
@@ -1,1 +1,16 @@
 
+def subNumbers(num1, num2):
+    num1 = 0
+    num2 = 0
+    output = num1 - num2
+    round(output, 3)
+
+def  isEven(Number):
+    Number = 0
+    if (Number % 2) == 0:
+        isEven = True
+    else:
+      isEven = False
+
+def  isMultiple(Number, MultipleOf):
+    if 
---
+++ TO DO/functions/functions and libraries/myTools.py
@@ -0,0 +1,12 @@
+def getNumber(Msg,Min, Max,Type):
+    number=input("enter a number")
+    
+
+def makeChange():
+    return
+
+def getChange():
+    return
+
+def isPrime():
+    return
