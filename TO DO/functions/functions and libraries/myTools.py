9d5f89c97c54c926a143bc6ec9bd1897a930926c
Pro_Fire_Development <zakm4@educbe.ca>
2021-06-12 21:16:08 -0600

    added in all of the detailed comments

--- TO DO/functions/functions and libraries/myTools.py
+++ TO DO/functions/functions and libraries/myTools.py
@@ -1,6 +1,23 @@
 # import the modules
 import math
 
+# asks the user to input a number and returns that number as an integer or floating 
+# point number, depending on what the mode is set to (default should be integer). 
+# It should have a required parameter called Msg that contains the string that will be 
+# displayed when the user is asked for input. The function must have an error catching code to protect 
+# against bad inputs. It should also have optional minimum and maximum parameters for the range the 
+# number should be in and ensure that the inputted number is within that range before returning it. 
+# Note that the function should allow for just a minimum or maximum value to be specified.
+#
+#Parameters:
+
+#Max = None (optional)
+#Type = Int (optional)
+#Msg (required)
+
+#Returns an integer or a floating point number, depending on what Type is set to.
+
+
 def getNumber(Msg,Min =1, Max= 10,Type = int):
     # ^define the perams
     # for int defults
@@ -46,8 +63,19 @@ def getNumber(Msg,Min =1, Max= 10,Type = int):
             except ValueError:
                 print("This is not a float (decimal).") # more type checking, to make sure you cant just use a string
 
-# time to make some change
 
+
+#takes in a decimal number representing some amount of money and returns how many of each 
+# type of bill and coin are needed to equal that amount as a string
+
+# Parameters:
+
+# Amount 
+
+# Returns a formatted string containing what amounts of each coin and bill should 
+# be given to equal the amount given.
+
+#lets make some change!
 def makeChange(Amount):
       try:
           NewAmount =math.modf(Amount) # takes the ammount, makes a tuple so that the int and then float can be
@@ -107,6 +135,21 @@ def makeChange(Amount):
                  quit()
 
 
+#takes in two parameters, how much a person owes and how much a person paid in cash,
+#  return what bill and and coins they should receive as change and returns it as a string. 
+# This function should call the makeChange() function to calculate what change should be given.
+
+#Hint: Use integer division (//) and modulo (%) functions to calculate how many of each 
+# denomination should be given as change
+
+#Parameters:
+
+#Owed, Paid (required)
+
+#Returns the same as getChange, but for the difference between the amount owed and 
+# the amount paid
+
+
 def getChange(Owed, Paid):
     try:
         # finds the total owed by taking how much was paid and them subtracting the cost of goods, assumes
@@ -159,6 +202,19 @@ def getChange(Owed, Paid):
      except:
       print("exit code 1, a fatal error has occurred") #if it fails then it will exit and quit.
       quit()
+
+
+#takes in an integer number as a parameter and returns True if that number is a 
+# prime number and False if it is not. Note that there are several opportunities to 
+# improve the performance of this function so that it runs faster. Without optimization, 
+# this function will be VERY slow when applied to large numbers.
+
+#Parameters:
+
+#Number (required)
+
+#Returns True or False, depending on whether number is prime or not.
+
 
 #returns if true or false or not, baised on if the number is prime
 # used algorithum
