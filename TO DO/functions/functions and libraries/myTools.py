7d06ced9ae775e684eff988e440afef95d87738a
Pro_Fire_Development <zakm4@educbe.ca>
2021-06-12 21:07:48 -0600

    finished comments

--- TO DO/functions/functions and libraries/myTools.py
+++ TO DO/functions/functions and libraries/myTools.py
@@ -1,14 +1,21 @@
+# import the modules
 import math
 
 def getNumber(Msg,Min =1, Max= 10,Type = int):
+    # ^define the perams
     # for int defults
     if(Type==int):
         try:
+            # trys to get an inital input
             value=int(input(Msg))
+            # checks for max val or if equal
             if(value<=Max):
+                # checks to see if numver is above minimum value
                 if(value>=Min):
                     return value
-
+                    
+                    # value error exceptions for imput is not a number or the right number
+                    # according to the type
         except ValueError:
             print("This is not a whole number.")
             try:
@@ -19,13 +26,14 @@ def getNumber(Msg,Min =1, Max= 10,Type = int):
                         if(value<=Max):
                             if(value>=Min):
                                 return value
+                            # checks if a float was entered instead as a way to catch that
                     except ValueError:
                         print("This is not a Float (decimal).")
             except:
                 print("This is not a proper number, please format it correctly.")
-
+                # if its not the defult value, then it auto assumes its a float and treats it accordingly
     elif(Type==float):
-        try:
+        try: # does all the same things as before, just as a float
             value=float(input(Msg))
             if(value<=Max):
                 if(value>=Min):
@@ -36,15 +44,17 @@ def getNumber(Msg,Min =1, Max= 10,Type = int):
             try:
                 value=int(input("Type a number:"))
             except ValueError:
-                print("This is not a float (decimal).")
+                print("This is not a float (decimal).") # more type checking, to make sure you cant just use a string
 
+# time to make some change
 
 def makeChange(Amount):
       try:
-          NewAmount =math.modf(Amount)
-          #dec first, doller second
-          b=int(Amount[1]) # b = bills
-            #bills
+          NewAmount =math.modf(Amount) # takes the ammount, makes a tuple so that the int and then float can be
+          # prosessed seprarely
+          # float 0, int 1
+          b=int(Amount[1]) # b = bills 
+            # bills
           hundred = b//100.00
           b = b%100.00
           fifty  = b//50.00
@@ -57,8 +67,9 @@ def makeChange(Amount):
           b = b%5.00
           one  = b//1.00
           b = b%1.00
-
-          #coins
+          # all of the above are the diffrent demomanations of bills that it checks for
+          
+          # coins
           c=float(NewAmount[0])
           quarters = c// 0.25
           c = c%0.25
@@ -69,34 +80,46 @@ def makeChange(Amount):
           pennies = c//0.01
           c =c%0.05
 
-          #printing out of the values:
+          # all of the above are the diffrent demomanations of bills that it checks for
+          # printing out of the values:
 
+          # takes all of the bill values, lables them and then gets ready to output them
           Dvalues = f" the change is:\n DOllARS:\n $1: {one}\n $5: {five}\n $10: {ten}\n $20: {twenty}\n $50: {fifty}\n $100: {hundred}"
           Cvalues =f"CENTS:\n $0.01: {pennies}\n $0.05: {nickels}\n $0.10: {dimes}\n $0.25: {quarters}"
-
+          # this one does the same thing for the coins
+          
+          # adds them together seprately
           results = Dvalues + "\n" + Cvalues
+          # puts them into one final string
+
+           #this could of all been done on one line or in one varm, but this seemed more human readable to me
           return results
 
       except:
-         error = input("an error has occoured counting\n are you sure you entered a string?\n try again? (Y or N)")
+         error = input("an error has occoured counting\n are you sure you entered a value?\n try again? (Y or N)")
+         # some more basic error counting to make sure that its money being entered
          if(error=="Y"):
              try:
-              makeChange()
+              makeChange() # trys to run the function again from the beginning.
              except:
                  print("exit code 1, a fatal error has occurred")
-         else:
-             quit()
+                 # something went wrong, so it exits and quits
+                 quit()
 
 
 def getChange(Owed, Paid):
     try:
+        # finds the total owed by taking how much was paid and them subtracting the cost of goods, assumes
+        # that either both are the same (IE 0 money to pay out) or that the customer over paid and thats why you
+        # need to pay out change
+
           TOTALOWED = Paid-Owed
           NewAmount =math.modf(TOTALOWED)
-          #dec first, doller second
+          # dec first, doller second
           b=int(Paid[1]) # b = bills
-            #bills
+            # bills
           hundred = b//100.00
-          b = b%100.00
+          b = b% 100.00
           fifty  = b//50.00
           b = b%50.00
           twenty = b//20.00
@@ -108,7 +131,7 @@ def getChange(Owed, Paid):
           one  = b//1.00
           b = b%1.00
 
-          #coins
+          # coins
           c=float(NewAmount[0])
           quarters = c// 0.25
           c = c%0.25
@@ -118,23 +141,26 @@ def getChange(Owed, Paid):
           c = c%0.05
           pennies = c//0.01
           c =c%0.05
+          # this part is all basicly the same as before (for the most part)
           Dvalues = f" the change is:\n DOllARS:\n $1: {one}\n $5: {five}\n $10: {ten}\n $20: {twenty}\n $50: {fifty}\n $100: {hundred}"
           Cvalues =f"CENTS:\n $0.01: {pennies}\n $0.05: {nickels}\n $0.10: {dimes}\n $0.25: {quarters}"
+          # this is the exact same as before too
           changegiven = Dvalues + "\n" + Cvalues
-
-          return "we Owe you ${TOTALTOTALOWED}\n" + changegiven
+          # just changed the names here
+          return "we Owe you ${TOTALTOTALOWED}\n" + changegiven #show the math from before and then the print out of what bills /coins 
+          # are needed to pay the customer out
 
     except:
        error = input("an error has occoured counting\n are you sure you entered a string?\n try again? (Y or N)")
     if(error=="Y"):
+        #more checking to make sure that this is extra safe, especially since this deals with money.
      try:
-       getChange()
+       getChange() #incase of failure, trys to re run it again
      except:
-      print("exit code 1, a fatal error has occurred")
-     else:
+      print("exit code 1, a fatal error has occurred") #if it fails then it will exit and quit.
       quit()
 
-
+#returns if true or false or not, baised on if the number is prime
 # used algorithum
 def isPrime(Number):
      # We know 1 is not a prime number
